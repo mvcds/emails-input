1d09a482909e4923a34a67879ffb03a081d7556a
Marcos Vinícius C. da Silva <marcos.silva@chamatheapp.com>
2020-07-11 16:45:51 +0200

    remove backtick for ie11

--- docs/emails-input.umd.js
+++ docs/emails-input.umd.js
@@ -825,9 +825,7 @@
     return re.test(String(email).toLowerCase());
   }
 
-  var remove = `<svg viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z" fill="#050038"/>
-</svg>`;
+  var remove = '<svg viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0.8L7.2 0L4 3.2L0.8 0L0 0.8L3.2 4L0 7.2L0.8 8L4 4.8L7.2 8L8 7.2L4.8 4L8 0.8Z" fill="#050038"/></svg>';
 
   function addBlock(_ref, event) {
     var editor = _ref.editor,
